57f6b52e13d74c09e5ff424ed2990aea33bdc4e0
LiuJian <jianliu517@163.com>
2017-06-02 21:43:49 +0800

    Update pat_user_crawler.py
    add comments

--- pat_user_crawler.py
+++ pat_user_crawler.py
@@ -1,12 +1,20 @@
 '''
 Author:LiuJian
 Date:2017-06-01
+
+技术路线：
+1.根据PAT排名页面下载所有用户排名列表（但排名列表显示的为昵称）
+2.通过每条数据的链接，进入个人主页
+3.获取用户名
 '''
 import requests
 from bs4 import BeautifulSoup
 import bs4
 
 def get(url):
+    '''
+    根据页面URL下载内容
+    '''
     try:
         # r = requests.get("http://www.baidu.com")
         r = requests.get(url)
@@ -17,9 +25,9 @@ def get(url):
         return ''
 
 
-
+# PAT排名页面地址
 url_base = "https://www.patest.cn/contests/pat-a-practise/ranklist?page="
-
+# PAT用户主页地址前缀
 user_url_base = "https://www.patest.cn"
 user = []
 
@@ -33,24 +41,26 @@ def get_user(url):
     for tr in table.children:
         if isinstance(tr,bs4.element.Tag):
             tds = tr('td')
-            u = tds[1].find('a').get('href')
+            u = tds[1].find('a').get('href')  # 用户主页地址后缀
             
-            user_url = user_url_base + u
+            user_url = user_url_base + u      # 与前缀拼接成用户主页URL
             content = get(user_url)
             soup = BeautifulSoup(content,'html.parser')
             tb = soup.find('tbody')
             tr = tb.find('tr')
             td = tr.find('td')
-            user.append(td.string)
+            user.append(td.string)            # 第一个td标签当中的内容为：用户名
 
 
 if __name__=="__main__":
-    num = 293
+    num = 293  # 当前PAT甲级排名的页数
     for i in range(1,num+1):
         url = url_base + str(i)
         get_user(url)
         print(r"alredy done:{} of {}".format(i,num))
     # print(user)
+    
+    # 将所有用户名存入文件
     with open("PAT_USER.txt",'w') as f:
         for i in user:
             try:
